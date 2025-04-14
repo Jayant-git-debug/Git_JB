3f58bc54eac868e76dd97c7209b92f7d94dbd6fa
Jayant-git-debug <jayantbasone100@yahoo.com>
2025-04-14 20:23:23 +0530

    Second day task completed

---
+++ Day-02-Task.java
@@ -0,0 +1,14 @@
+public class Addition {
+	    public static void main(String[] args) {
+		            // Declare and initialize two numbers
+			    //         int num1 = 10;
+			    //                 int num2 = 20;
+			    //
+			    //                         // Add the numbers
+			    //                                 int sum = num1 + num2;
+			    //
+			    //                                         // Display the result
+			    //                                                 System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+			    //                                                     }
+			    //                                                     }
+			    //
